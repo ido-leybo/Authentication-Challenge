562b25a5d653ab6401ef34696916ed07cdb3122d
Ido Leybovitch <idoleybo01@gmail.com>
2021-04-27 22:01:29 +0300

    pass all the tests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const { hashSync, genSaltSync, compareSync } = require("bcrypt");
+const e = require("express");
 const app = express();
 app.use(express.json());
 
@@ -49,10 +50,8 @@ app.post("/users/login", (req, res) => {
       return res.status(403).send("User or Password incorrect");
     }
   }
-  const accessToken = jwt.sign({ userData }, "secretKey", { expiresIn: "10s" });
-  const refreshToken = jwt.sign({ userData }, "secretRefreshKey", {
-    expiresIn: "1d",
-  });
+  const accessToken = jwt.sign({ userData }, "secretKey", { expiresIn: 10 });
+  const refreshToken = jwt.sign({ userData }, "secretRefreshKey");
   REFRESHTOKENS.push(refreshToken);
   res.status(200).json({
     accessToken: accessToken,
@@ -63,38 +62,25 @@ app.post("/users/login", (req, res) => {
   });
 });
 
-app.get("/api/v1/information", (req, res) => {
-  const bearerToken = req.get("authorization");
-  if (!bearerToken) {
-    return res.status(401).send("Access Token Required");
-  }
-  const token = bearerToken.slice(7);
-  const user = jwt.verify(token, "secretKey");
+app.get("/api/v1/information", verifyToken, (req, res) => {
+  const user = req.user;
+  console.log(user);
+  console.log(INFORMATION);
 
-  if (!user.userData) {
-    return res.status(403).send("Invalid Access Token");
-  }
-  const userInfo = INFORMATION.filter(
-    (item) => item.email === user.userData.email
-  );
-  res.status(200).json(userInfo);
+  const userInfo = INFORMATION.filter((item) => {
+    console.log(item);
+    console.log(user);
+    return item.email === user.email;
+  });
+  console.log(userInfo);
+  res.status(200).send(userInfo);
 });
 
-app.get("/api/v1/users", (req, res) => {
-  const bearerToken = req.get("authorization");
-
-  if (!bearerToken) {
-    return res.status(401).send("Access Token Required");
-  }
-  const token = bearerToken.slice(7);
-  //   console.log(token);
-  const user = jwt.verify(token, "secretKey");
-
-  if (!user.userData) {
-    return res.status(403).send("Invalid Access Token");
-  }
-
-  res.status(200).json(USERS);
+app.get("/api/v1/users", verifyToken, (req, res) => {
+  const user = req.user;
+  const adminValid = USERS.find((i) => i.email === user.email);
+  if (!adminValid.isAdmin) return res.status(403).send("Not Required");
+  res.status(200).send(USERS);
 });
 
 app.post("/users/logout", (req, res) => {
@@ -111,15 +97,172 @@ app.post("/users/logout", (req, res) => {
   res.status(200).send("User Logged Out Successfully");
 });
 
-// function verifyToken(req, res, next) {
-//   const bearerHeader = req.headers["authorization"];
-//   if (typeof bearerHeader !== "undefined") {
-//     const bearer = bearerHeader.split(" ");
-//     const bearerToken = bearer[1];
-//     req.token = bearerToken;
-//     next();
-//   } else {
-//     res.sendStatus(403);
-//   }
-// }
+app.post("/users/token", async (req, res) => {
+  const refreshToken = req.body.token;
+  if (!refreshToken) return res.status(401).send("Refresh Token Required");
+  const currentToken = REFRESHTOKENS.find((token) => token === refreshToken);
+  if (!currentToken) return res.status(403).send("Invalid Refresh Token");
+  jwt.verify(currentToken, "secretRefreshKey", (err, authData) => {
+    if (err) {
+      return res.status(403).send("Not Required");
+    } else {
+      const accessToken = jwt.sign(authData.userData, "secretKey", {
+        expiresIn: 10,
+      });
+      return res.status(200).json({ accessToken });
+    }
+  });
+});
+
+app.options("/", (req, res) => {
+  const bearerHeader = req.headers["authorization"];
+  if (!bearerHeader) {
+    return res.status(200).send([
+      {
+        method: "post",
+        path: "/users/register",
+        description: "Register, Required: email, name, password",
+        example: {
+          body: { email: "user@email.com", name: "user", password: "password" },
+        },
+      },
+      {
+        method: "post",
+        path: "/users/login",
+        description: "Login, Required: valid email and password",
+        example: { body: { email: "user@email.com", password: "password" } },
+      },
+    ]);
+  } else {
+    const token = bearerHeader.split(" ")[1];
+    jwt.verify(token, "secretKey", (err, authData) => {
+      if (err) {
+        return res.status(200).send([
+          {
+            method: "post",
+            path: "/users/register",
+            description: "Register, Required: email, name, password",
+            example: {
+              body: {
+                email: "user@email.com",
+                name: "user",
+                password: "password",
+              },
+            },
+          },
+          {
+            method: "post",
+            path: "/users/login",
+            description: "Login, Required: valid email and password",
+            example: {
+              body: { email: "user@email.com", password: "password" },
+            },
+          },
+          {
+            method: "post",
+            path: "/users/token",
+            description: "Renew access token, Required: valid refresh token",
+            example: { headers: { token: "*Refresh Token*" } },
+          },
+        ]);
+      }
+      const adminValid = USERS.find((i) => i.email === authData.userData.email);
+      if (!adminValid.isAdmin) {
+        return res.status(200).send([
+          {
+            method: "post",
+            path: "/users/register",
+            description: "Register, Required: email, name, password",
+          },
+          {
+            method: "post",
+            path: "/users/login",
+            description: "Login, Required: valid email and password",
+          },
+          {
+            method: "post",
+            path: "/users/token",
+            description: "Renew access token, Required: valid refresh token",
+          },
+          {
+            method: "get",
+            path: "/api/v1/information",
+            description:
+              "Access user's information, Required: valid access token",
+          },
+          {
+            method: "post",
+            path: "/users/logout",
+            description: "Logout, Required: access token",
+          },
+          {
+            method: "post",
+            path: "/users/tokenValidate",
+            description:
+              "Access Token Validation, Required: valid access token",
+          },
+        ]);
+      } else {
+        return res.status(200).send([
+          {
+            method: "post",
+            path: "/users/register",
+            description: "Register, Required: email, name, password",
+          },
+          {
+            method: "post",
+            path: "/users/login",
+            description: "Login, Required: valid email and password",
+          },
+          {
+            method: "post",
+            path: "/users/token",
+            description: "Renew access token, Required: valid refresh token",
+          },
+          {
+            method: "post",
+            path: "/users/tokenValidate",
+            description:
+              "Access Token Validation, Required: valid access token",
+          },
+          {
+            method: "get",
+            path: "/api/v1/information",
+            description:
+              "Access user's information, Required: valid access token",
+          },
+          {
+            method: "post",
+            path: "/users/logout",
+            description: "Logout, Required: access token",
+          },
+          {
+            method: "get",
+            path: "api/v1/users",
+            description:
+              "Get users DB, Required: Valid access token of admin user",
+          },
+        ]);
+      }
+    });
+  }
+});
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const token = bearerHeader.split(" ")[1];
+    jwt.verify(token, "secretKey", (err, authData) => {
+      if (err) {
+        return res.status(403).send("Invalid Access Token");
+      } else {
+        req.user = authData.userData ? authData.userData : authData;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).send("Access Token Required");
+  }
+}
+
 module.exports = app;
